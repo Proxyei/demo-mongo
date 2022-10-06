b5abef61b6151e0a0d5f70d4d4ebcdc60a00d83a
future2022 <10383726+future2022@user.noreply.gitee.com>
2022-10-06 15:55:27 +0800

    去掉自定义配置

--- src/main/java/com/xywei/springboot/demomongo/DemoMongoApplication.java
+++ src/main/java/com/xywei/springboot/demomongo/DemoMongoApplication.java
@@ -2,8 +2,9 @@ package com.xywei.springboot.demomongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = MongoAutoConfiguration.class)
 public class DemoMongoApplication {
 
     public static void main(String[] args) {
